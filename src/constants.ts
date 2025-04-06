8e7674814f0b9038a6c47ee140de68ab24c5758f
Benoit Bénézech <benoit.benezech@gmail.com>
2025-04-06 23:32:12 +0200

    Make bullets fly faster but nearer

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const TILE_HEIGHT_PX = 16;
-export const SMALL_WORLD_FACTOR = 8;
-export const PIXELS_PER_METER = 8; // Pixels per meter
+export const SMALL_WORLD_FACTOR = 12;
+export const PIXELS_PER_METER = 12; // Pixels per meter
 export const ENEMY_SPRITE = "enemy"; // Enemy sprite key
 export const CANNON_SPRITE = "cannon"; // Cannon sprite key
 export const BULLET_SPRITE = "bullet"; // Bullet sprite key
