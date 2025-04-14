3fe1cda68c215eac8f99eb4f5a14847e8f1bee3e
Benoit Bénézech <benoit.benezech@gmail.com>
2025-04-14 12:32:24 +0200

    Separate game logic from graphics

--- src/Bullet.ts
+++ src/Bullet.ts
@@ -18,17 +18,17 @@ const C_d = 0.5;
 const rho = 1.225; // Air Density (rho): Standard sea-level density ≈ 1.225 kg/m³
 
 export class Bullet extends Phaser.GameObjects.Image {
-  private _screen: Phaser.Math.Vector2 = new Phaser.Math.Vector2(0, 0);
-  private _shadowScreen: Phaser.Math.Vector2 = new Phaser.Math.Vector2(0, 0);
-  private _shadowWorld: Phaser.Math.Vector3 = new Phaser.Math.Vector3(0, 0, 0);
+  private _screen: Phaser.Math.Vector2 = new Phaser.Math.Vector2();
+  private _shadowScreen: Phaser.Math.Vector2 = new Phaser.Math.Vector2();
+  private _shadowWorld: Phaser.Math.Vector3 = new Phaser.Math.Vector3();
 
   private moveTimer = 0;
 
+  world: Phaser.Math.Vector3;
+  velocity: Phaser.Math.Vector3;
   shadowSprite: Phaser.GameObjects.Image;
   gameScene: GameScene;
   dragFactor: number;
-  world: Phaser.Math.Vector3;
-  velocity: Phaser.Math.Vector3;
 
   constructor(
     gameScene: GameScene,
@@ -51,7 +51,11 @@ export class Bullet extends Phaser.GameObjects.Image {
       PIXELS_PER_METER;
 
     this.gameScene.add.existing(this);
-    this.shadowSprite = gameScene.add.sprite(0, 0, BULLET_SHADOW_SPRITE);
+    this.shadowSprite = gameScene.add
+      .image(0, 0, BULLET_SHADOW_SPRITE)
+      .setAlpha(0.5);
+
+    this.updateVisuals();
   }
 
   groundElevation(): number {
@@ -60,26 +64,18 @@ export class Bullet extends Phaser.GameObjects.Image {
     return this.world.z - surfaceZ;
   }
 
-  getShadowWorld(): Phaser.Math.Vector3 | null {
+  getShadowScreen(): Phaser.Math.Vector2 | null {
+    const surfaceZ = this.gameScene.getSurfaceZFromWorldPosition(this.world);
+    if (surfaceZ === null) return null; // No surface, return null
+
     this._shadowWorld.x = this.world.x;
     this._shadowWorld.y = this.world.y;
-    const surfaceZ = this.gameScene.getSurfaceZFromWorldPosition(this.world);
+    this._shadowWorld.z = surfaceZ;
 
-    this._shadowWorld.y = this.world.y;
-    if (surfaceZ === null) {
-      return null; // No surface, return null
-    } else {
-      this._shadowWorld.z = surfaceZ;
-    }
-
-    return this._shadowWorld;
-  }
-
-  getShadowScreen(): Phaser.Math.Vector2 | null {
-    const shadowWorld = this.getShadowWorld();
-    if (shadowWorld === null) return null; // No surface, return null
-
-    return this.gameScene.getScreenPosition(shadowWorld, this._shadowScreen);
+    return this.gameScene.getScreenPosition(
+      this._shadowWorld,
+      this._shadowScreen
+    );
   }
 
   move(delta: number) {
@@ -122,13 +118,15 @@ export class Bullet extends Phaser.GameObjects.Image {
 
     const surfaceZ =
       this.gameScene.getSurfaceZFromWorldPosition(this.world) ?? 0; // null means the ground is behind a building, let's assume 0 for now
+
+    const relativeZ = Math.max(0, this.world.z - surfaceZ);
     // Stop if slow and at ground
-    if (speed < 10 && this.world.z <= surfaceZ) {
+    if (speed < 10 && relativeZ === 0) {
       this.destroy();
       return;
     }
 
-    if (this.world.z <= surfaceZ && this.velocity.z < 0) {
+    if (relativeZ === 0 && this.velocity.z < 0) {
       // Moving down and at/below ground
       // Correct position
       this.world.z = surfaceZ;
@@ -140,21 +138,20 @@ export class Bullet extends Phaser.GameObjects.Image {
       this.velocity.x *= GROUND_FACTOR;
       this.velocity.y *= GROUND_FACTOR;
     }
+  }
 
+  updateVisuals() {
     const screen = this.gameScene.getScreenPosition(this.world, this._screen);
-    this.setPosition(screen.x, screen.y)
-      .setScale(1 + this.world.z * 0.003)
-      .setDepth(screen.y);
+    this.setPosition(screen.x, screen.y);
 
     const shadowScreen = this.getShadowScreen();
     if (shadowScreen === null) {
-      this.shadowSprite.setAlpha(0);
-    } else
-      this.shadowSprite
-        .setPosition(shadowScreen.x, shadowScreen.y)
-        .setScale(1 + this.world.z * 0.005)
-        .setAlpha(Math.max(0.1, 0.5 - this.world.z * 0.003))
-        .setDepth(Math.round(screen.y) - 1);
+      this.shadowSprite.setVisible(false);
+    } else {
+      if (this.shadowSprite.visible === false)
+        this.shadowSprite.setVisible(true);
+      this.shadowSprite.setPosition(shadowScreen.x, shadowScreen.y);
+    }
   }
 
   preUpdate(time: number, delta: number) {
@@ -165,6 +162,7 @@ export class Bullet extends Phaser.GameObjects.Image {
     this.moveTimer += delta;
     if (this.moveTimer >= timerInterval) {
       this.move(this.moveTimer);
+      if (visible) this.updateVisuals();
       this.moveTimer = 0;
     }
   }
