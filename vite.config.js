42ed2281e954e6e29b339462cd41d872c9be6cce
Benoit Bénézech <benoit.benezech@gmail.com>
2025-04-15 20:41:23 +0200

    Fix Github page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
   plugins: [],
+  base: mode === "gh-pages" ? `/helm-defence/` : "/",
   server: { host: "0.0.0.0", port: 9000 },
   clearScreen: false,
-});
+}));
